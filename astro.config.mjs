c541ab18af203d0fa09d7a6fb2249b3693afef0f
jafeth <jafethrafael12@gmail.com>
2024-08-24 01:22:47 +0200

    auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import auth from "auth-astro";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), auth()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
