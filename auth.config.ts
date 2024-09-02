5c52801cfb839f635b546d8daec61f62c0f24d87
jafeth <jafethrafael12@gmail.com>
2024-09-02 19:52:08 +0200

    random fixes

--- auth.config.ts
+++ auth.config.ts
@@ -7,8 +7,7 @@ export default defineConfig({
 		Spotify({
 			clientId: import.meta.env.SPOTIFY_CLIENT_ID,
 			clientSecret: import.meta.env.SPOTIFY_CLIENT_SECRET,
-            authorization: { params: { scope: "user-top-read user-read-recently-played user-read-private" } },
-            // scope: "user-top-read user-read-recently-played user-read-private user-read-currently-playing"
+            authorization: { params: { scope: "user-top-read user-read-recently-played user-read-private user-read-currently-playing" } },
 		}),
 	],
     callbacks: {
